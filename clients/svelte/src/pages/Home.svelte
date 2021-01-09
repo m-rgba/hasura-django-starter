dd5fd6436d40330e27f1a23ad9a5c8f503ab88db
Martin Mark <martin@martinmark.com>
2021-01-09 13:23:37 -0500

    Client Update / Readme (#14)
    * HTML Mockups
    
    * Cleanup
    
    - Cleanup docs
    - Cleanup requirements.txt (Django)
    - Moved hoppscotch to seperate compose file
    - Updated layout for frontend
    
    * Django secrets docs
    
    - Update secret section to add command to create new secret
    - Rotate secret for urlsafe secret
    
    * Svelte Auth Logic > Readme Update
    
    * Updated JWT logic > backend user check
    
    * - Refactored Django docker process
    - Re-added Django migrations > removed initial Hasura migration (do the same thing)
    - Continued Svelte model
    
    * Client-Side Update
    
    Svelte
    - First Draft
    
    Migrations
    - Updated ID + FKs
    - Updated unique to email
    
    * Update README.md
    
    * Client
    
    - Fixed on keyup
    - Removed console.log
    - Readme:Svelte + screenshot
    
    * Metadata Fixes
    
    - Hotfixes to metadata
    - Added a function and view example
    
    * Updated Client
    
    - Client refactor > less spaghetti 🍝
    - Allow `user` group to edit own profile
    - Added last_login update from client
    
    * Update Client > Subscription Starter
    
    - Fixed a loop in login
    - Added a realtime demo table
    - Renamed other tables demo
    - Added first screen for subscriptions
    
    * Added Realtime
    
    - Added appolo subscription view
    - Cleaned up migrations
    - Added Manager
    - Fixed some permission issues
    
    * Client Update / Readme
    
    - Make realtime more resilient for Hasura Admin users
    - Styling tweaks
    - Update readme for realtime section

--- clients/svelte/src/pages/Home.svelte
+++ clients/svelte/src/pages/Home.svelte
@@ -15,8 +15,14 @@
     import Header from "../components/Header.svelte"
     import Footer from "../components/Footer.svelte"
 
+    let user_role = localStorage.getItem("user_role");
     let username = localStorage.getItem("user_name");
+    let user_id = localStorage.getItem("user_id");
+
     let message;
+
+    let successMessage;
+    let errorMessage;
 
     setClient(client);
     const MESSAGES = gql`
@@ -34,15 +40,31 @@
     const messageSubscribe = subscribe(client, { query: MESSAGES });
 
     async function sendMessage() {
+        let query;
         const variable = {};
-        const query = `
-            mutation updateUserStatus($message: String!) {
-                insert_demo_realtime_one(object: {message: $message}) {
-                    created_at
-                    message
+        // Other users are restricted for defining their own 'created_by' > added so the feature doesn't break for Hasura Admins (which dont auto-populate).
+        if(user_role === "admin"){
+            query = `
+                mutation updateUserStatus($message: String!, $created_by: Int) {
+                    insert_demo_realtime_one(object: {created_by: $created_by, message: $message}) {
+                        created_at
+                        message
+                    }
                 }
-            }
-        `;
+            `; 
+        } else {
+            query = `
+                mutation updateUserStatus($message: String!) {
+                    insert_demo_realtime_one(object: {message: $message}) {
+                        created_at
+                        message
+                    }
+                }
+            `; 
+        }
+        if(user_role === "admin"){
+            variable["created_by"] = user_id;
+        }
         variable["message"] = message;
         const accessToken = await token();
         const request = await fetch("http://localhost:8080/v1/graphql", {
@@ -51,9 +73,9 @@
             body: JSON.stringify({ query: query, variables: variable })
         });
         const sendMessage = await gqlResponseHandler(request);
-        console.log(sendMessage);
         if (sendMessage.success === true){
-            console.log(sendMessage.response);
+            console.log("> Message sent");
+            // console.log(sendMessage.response);
         } else {
             errorMessage = sendMessage.response;
         }  
@@ -94,13 +116,30 @@
 
 <Header />
 
+<div class="notification-area">
+    {#if successMessage}
+        <div class="notification mb-xs positive">
+            <a on:click={() => successMessage = ''} style="color:#fff; float:right;" href="#!"><ion-icon name="close"></ion-icon></a>
+            <p>{successMessage}</p>
+        </div>
+    {/if}
+    {#if errorMessage}
+        <div class="notification mb-xs negative">
+            <a on:click={() => errorMessage = ''} style="color:#fff; float:right;" href="#!"><ion-icon name="close"></ion-icon></a>
+            <p>{errorMessage}</p>
+        </div>   
+    {/if}
+</div>
+
 <content>
     <div id="chat-container" class="container pt-xl">
         <div class="row mb-sm">
             <div class="col-3"> </div>
             <div class="col-6">
-                <h1 class="mb-xs">Realtime Test</h1>
-                <p class="muted">Back to Users</p>
+                <h1 class="mb-xs">Realtime Demo</h1>
+                <p class="muted">This is a demonstration of the realtime capabilities of Hasura's GraphQL API.</p>
+                <p class="muted">Send a message as another account and you'll be able to see messages stream in.</p>
+                <p class="muted">On the client side we're using svelte-apollo to display the results.</p>
             </div>
         </div>
         <div class="row">
