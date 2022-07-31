1609530bdf02de9f3718264cc284b8b3a69ed033
mourtallah <serigne.faye@icloud.com>
2022-07-31 13:43:26 -0400

    register apps with admin

--- django/apps/transcriptions/models.py
+++ django/apps/transcriptions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from sympy import content
 
 class transcriptions(models.Model): 
-    video_id = models.CharField(max_length=64)
-    title = models.CharField()
-    descriptions = models.CharField()
-    content = models.CharField()
+    video_id = models.TextField(max_length=64)
+    title = models.TextField()
+    descriptions = models.TextField()
+    content = models.TextField()
